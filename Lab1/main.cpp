ed5dd1eab4bc5158dcdfb0900e9699666f080e11
oriklein195 <klein.ori98@gmail.com>
2016-04-11 10:14:24 -0700

    Update main.cpp

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,1 +1,16 @@
+#include "Currency.h"
+#include "Wallet.h"
+#include <iostream>
 
+int main(){
+  //User enters up to 5 pre-defined currencies
+  
+  //Random number generator to add and remove currencies.
+  
+  //print total value on wallet
+  
+  //remove all money and print
+  
+  system("pause");
+  return 0;
+}
