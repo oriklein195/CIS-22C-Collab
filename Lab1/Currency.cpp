34c72001d8359ea18a7fc71e9cf575df1356b93e
oriklein195 <klein.ori98@gmail.com>
2016-04-13 11:21:07 -0700

    Update Currency.cpp

--- Lab1/Currency.cpp
+++ Lab1/Currency.cpp
@@ -1,46 +1,7 @@
 #include "Currency.h"
-
+#include <string>
 using namespace std;
 
-Currency::Currency(){
-	wholeVal = 0;
-	fracVal = 0;
-}
-Currency::Currency(int w, int f){
-	wholeVal = w;
-	fracVal = f;
-	simplify();
-}
-Currency Currency::operator+(Currency& other){
-	wholeVal += other.wholeVal;
-	fracVal += other.fracVal;
-	simplify();
-	return *this;
-}
-Currency Currency::operator-(Currency& other){
-	Currency c = Currency(this->wholeVal, this->fracVal);
-	c.wholeVal -= other.wholeVal;
-	c.fracVal -= other.fracVal;
-	try {
-		c.simplify();
-	}
-	catch (string e) {
-		throw e;
-	}
-	wholeVal = c.wholeVal;
-	fracVal = c.fracVal;
-	return *this;
-}
-istream& Currency::operator>> (istream& is){
-
-}
-ostream& Currency::operator<< (ostream& os){
-
-}
-double Currency::getValue() const{
-	double value = 0;
-	return value;
-}
 void Currency::simplify(){
 	if (fracVal > 99){
 		wholeVal += fracVal / 100;
@@ -55,4 +16,49 @@ void Currency::simplify(){
 	if (wholeVal < 0 || fracVal < 0){
 		throw "Can't have negative " + wholeName;
 	}
+}
+Currency::Currency(){
+	wholeVal = 0;
+	fracVal = 0;
+}
+Currency::Currency(int w, int f){
+	wholeVal = w;
+	fracVal = f;
+	simplify();
+}
+Currency operator+(const Currency& first, const Currency& other){
+	int wV, fV;
+	wV = first.wholeVal + other.wholeVal;
+	fV = first.fracVal + other.fracVal;
+	Currency c = Currency(wV, fV);
+	c.simplify();
+	return c;
+}
+Currency operator-(const Currency& first, const Currency& other){
+	Currency c = Currency(first.wholeVal, first.fracVal);
+	c.wholeVal -= other.wholeVal;
+	c.fracVal -= other.fracVal;
+	try {
+		c.simplify();
+	}
+	catch (string e) {
+		throw e;
+	}
+	return c;
+}
+istream& operator>> (istream& is, Currency& c){
+	string wN, fN;
+	int wV, fV;
+	is >> wV >> wN >> fV >> fN;
+	c = Currency(wV, fV);
+	
+	return is;
+}
+ostream& operator<<(ostream& os, const Currency& c){
+	os << c.wholeVal << " " << c.wholeName << " " << c.fracVal << " " << c.fracName << endl;
+	return os;
+}
+double Currency::getValue() const{
+	double value = wholeVal + fracVal/100.0;
+	return value;
 }
