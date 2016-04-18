5c7fde698175fa8aadadaf3165d8509020e3d956
oriklein195 <klein.ori98@gmail.com>
2016-04-18 10:20:49 -0700

    Update Dollar.h

--- Dollar.h
+++ Dollar.h
@@ -2,7 +2,9 @@
 using namespace std;
 #ifndef DOLLAR_H
 #define DOLLAR_H
-
+/**
+ * Creates a Dollar class that is derived from Currency.
+ */
 class Dollar : public Currency{
 public:
 	Dollar() :Currency(){
