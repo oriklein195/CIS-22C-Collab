0ccf41deecc47baebf7bb5ec7add0e1116423c21
tomerlich <tomer.erlich2@gmail.com>
2016-05-10 16:11:14 -0700

    the new stuff

--- Lab3/Lab3/InfixCalc.h
+++ Lab3/Lab3/InfixCalc.h
@@ -6,23 +6,35 @@
 class InfixCalc{
 private:
 	int size;
-	Stack<char> operands;
+	Stack<int> operands;
 	Stack<char> operators;
 	char *expression;
+	int precedence(char c){
+		switch (c){
+		case '(': case ')':
+			return 0;
+			break;
+		case '+': case '-':
+			return 1;
+			break;
+		case '*': case '/':
+			return 2;
+			break;
+		}
+	}
 public:
 	InfixCalc(){
 		size = 0;
 		expression = nullptr;
-		operands = Stack<char>();
+		operands = Stack<int>();
 		operators = Stack<char>();
 	}
 	InfixCalc(int s, char* ex){
 		size = s;
 		for (int i = 0; i < size; i++)
 			expression[i] = ex[i];
-		operands = Stack<char>();
+		operands = Stack<int>();
 		operators = Stack<char>();
-
 	}
 };
 #endif // !INFIXCALC_H
