8a233bdf39093604c6529e57fcee132ab8f0493a
oriklein195 <klein.ori98@gmail.com>
2016-06-03 11:34:42 -0700

    Update Person.h

--- Lab4/Person.h
+++ Lab4/Person.h
@@ -1,5 +1,21 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+class Person{
+private:
+  string name;
+  string birthdate;
+public:
+  Person(){
+    name = "";
+    birthdate = "";
+  }
+  Person(string n, string year, string month, string day){
+    name = n;
+    birthdate = year+month+day;
+  }
+  
+}
 
-#endif /* !PERSON_H */
+
+#endif // !PERSON_H 
