932f4f67cf395f0dbf53b266bc02c1873870030d
oriklein195 <klein.ori98@gmail.com>
2016-06-03 11:42:21 -0700

    Update Person.h

--- Lab4/Person.h
+++ Lab4/Person.h
@@ -3,16 +3,36 @@
 
 class Person{
 private:
-  string name;
-  string birthdate;
+  bool byName;
 public:
+  string name, year, month, day;
+  string birthdate;
   Person(){
+    byName = true;
     name = "";
     birthdate = "";
   }
-  Person(string n, string year, string month, string day){
+  Person(bool bN, string n, string y, string m, string d){
+    byName = bN;
     name = n;
+    year = y;
+    month = m;
+    day = d;
     birthdate = year+month+day;
+  }
+  ostream& operator<<(ostream& os, const Person& p){
+	  os << p.name << ", " << p.month << "/" << p.day << "/" << p.year;
+	  return os;
+  }
+  bool operator< (Person &p1, Person &p2){
+    if (byName)
+      return (p1.name < p2.name);
+    return (p1.birthdate < p2.birthdate);
+  }
+  bool operator> (Person &p1, Person &p2){
+    if (byName)
+      return (p1.name > p2.name);
+    return (p1.birthdate > p2.birthdate);
   }
   
 }
