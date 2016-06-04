646150fa2438f63301728cefc2afbd462c21c0c9
oriklein195 <klein.ori98@gmail.com>
2016-06-03 17:25:00 -0700

    Update Person.h

--- Lab4/Person.h
+++ Lab4/Person.h
@@ -1,51 +1,46 @@
 #ifndef PERSON_H
 #define PERSON_H
-#include <string>
 #include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
 
 class Person{
-private:
-  bool byName;
-  string name, year, month, day;
-  string birthdate;
 public:
-  
-  Person(){
-    byName = true;
-    name = "";
-    birthdate = "";
-  }
-  friend ostream& operator<<(ostream& os, const Person& p);
-  Person(bool bN, string n, string y, string m, string d){
-    byName = bN;
-    name = n;
-    year = y;
-    month = m;
-    day = d;
-    birthdate = year+month+day;
-  }
-
-  bool operator< (Person &p1){
-    if (byName)
-      return (name < p1.name);
-    return (birthdate < p1.birthdate);
-  }
-  bool operator> (Person &p1){
-    if (byName)
-      return (name > p1.name);
-    return (birthdate > p1.birthdate);
-  }
-  string getName(){ return name; }
-  string getYear(){ return year; }
-  string getMonth(){ return month; }
-  string getDay(){ return day; }
-  string getBirthdate() { return birthdate; }
-  
+    bool byName;
+    string name, year, month, day;
+    string birthdate;
+    Person(){
+        byName = true;
+        name = "";
+        birthdate = "";
+    }
+    Person(bool bN, string n, string y, string m, string d){
+        byName = bN;
+        name = n;
+        year = y;
+        month = m;
+        day = d;
+        birthdate = year+month+day;
+    }
+    friend ostream& operator<<(ostream& os, const Person& p);
+    friend bool operator< (const Person &p1, const Person &p2);
+    friend bool operator> (const Person &p1, const Person &p2);
 };
 ostream& operator<<(ostream& os, const Person& p){
-	os << p.name << ", " << p.month << "/" << p.day << "/" << p.year;
-	return os;
+    os << p.name << ", " << p.month << "/" << p.day << "/" << p.year;
+    return os;
+};
+bool operator< (const Person &p1, const Person &p2){
+    if (p1.byName)
+        return (p1.name < p2.name);
+    return (p1.birthdate < p2.birthdate);
+};
+bool operator> (const Person &p1, const Person &p2){
+    if (p1.byName)
+        return (p1.name > p2.name);
+    return (p1.birthdate > p2.birthdate);
 };
 
-#endif // !PERSON_H 
+#endif // !PERSON_H
